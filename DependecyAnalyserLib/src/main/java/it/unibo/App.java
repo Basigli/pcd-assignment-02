2f999b0af9740372dc53dc0eb391bed972cdab98
Gabribrele <gabrielebasigli@gmail.com>
2025-04-23 09:52:42 +0200

    (feat) visitor: first working dependency collector

--- DependecyAnalyserLib/src/main/java/it/unibo/App.java
+++ DependecyAnalyserLib/src/main/java/it/unibo/App.java
@@ -1,10 +1,23 @@
 package it.unibo;
 
-/**
- * Hello world!
- */
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static final String FILE_PATH = "/Users/gabe/Documents/GitHub/pcd-assignment-02/DependecyAnalyserLib/src/main/java/it/unibo/App.java";
+    public static void main(String[] args) throws IOException {
+        CompilationUnit cu = StaticJavaParser.parse(Files.newInputStream(Paths.get(FILE_PATH)));
+
+        var declarations = new ArrayList<String>();
+        VoidVisitor<List<String>> visitor = new DependencyCollector();
+        visitor.visit(cu, declarations);
+        declarations.forEach(n -> System.out.println("Class or interface: " + n));
     }
 }
---
+++ DependecyAnalyserLib/src/main/java/it/unibo/DependencyCollector.java
@@ -0,0 +1,17 @@
+package it.unibo;
+
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+import java.util.List;
+
+public class DependencyCollector extends VoidVisitorAdapter<List<String>> {
+
+    @Override
+    public void visit(ClassOrInterfaceType declaration, List<String> collector) {
+        super.visit(declaration, collector);
+        collector.add(declaration.getNameAsString());
+    }
+
+}
